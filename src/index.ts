20957839a0f1fe4ea96071836f9ebec79436029a
MaxLucky25 <maksrad93@gmail.com>
2025-01-23 11:22:55 +0300

    add id for addresses

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from "express"
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -18,22 +18,22 @@ app.use(jsonBodyMiddleware)
 
 const db = {
     courses: [
-        {id:1, title: 'front-end'},
-        {id:2, title: 'back-end'},
-        {id:3, title: 'automation qa'},
-        {id:4, title: 'devops'}
+        {id:1, title: "front-end"},
+        {id:2, title: "back-end"},
+        {id:3, title: "automation qa"},
+        {id:4, title: "devops"}
     ]
 }
 
 
 // урок(11)
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   let helloMessage = "Hello incubator. I could make it"
     res.send(helloMessage)
 
 })
-app.get('/courses',(req, res)=>{
+app.get("/courses",(req, res)=>{
     let foundCourses = db.courses;
     if(req.query.title){
         foundCourses=foundCourses
@@ -48,7 +48,7 @@ app.get('/courses',(req, res)=>{
     
     res.json(foundCourses)
 })
-app.get('/courses/:id',(req, res)=>{
+app.get("/courses/:id",(req, res)=>{
     const foundCourses = db.courses.find(c => c.id === +req.params.id)
 
     if (!foundCourses){
@@ -59,7 +59,7 @@ app.get('/courses/:id',(req, res)=>{
     res.json(foundCourses);
 
 })
-app.post('/courses', (req,res) =>{
+app.post("/courses", (req,res) =>{
     if (!req.body.title) {
         res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
         return;
@@ -74,13 +74,13 @@ app.post('/courses', (req,res) =>{
         .status(HTTP_STATUSES.CREATED_201)
         .json(createdCourse)
 })
-app.delete('/courses/:id',(req, res)=>{
+app.delete("/courses/:id",(req, res)=>{
     db.courses = db.courses.filter(c => c.id !== +req.params.id)
 
     res.sendStatus(HTTP_STATUSES.NO_CONTENT_204);
 
 })
-app.put('/courses/:id',(req, res)=>{
+app.put("/courses/:id",(req, res)=>{
     if (!req.body.title) {
         res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400);
         return;
@@ -101,17 +101,25 @@ app.put('/courses/:id',(req, res)=>{
 
 // новый урок(12), новые endpoints
 const products =[{title:"tomato"},{title:"orange"}];
-const adresses= [{title:"Nezalejnasti 12"},{title:"Selickogo 11"}];
+const adresses= [{id:1, title:"Nezalejnasti 12"},{id:2, title:"Selickogo 11"}];
 
-app.get('/products',(req,res)=>{
+app.get("/products",(req,res)=>{
     res.send(products);
 })
-app.get('/products/:productTitle',(req,res)=>{
+app.get("/products/:productTitle",(req,res)=>{
     let product = products.find(p=>p.title === req.params.productTitle)
     res.send(product);
 })
-app.get('/adresses',(req,res)=>{
+app.get("/addresses",(req, res)=>{
     res.send(adresses);
+})
+app.get("/addresses/:id",(req, res)=>{
+    let address =adresses.find(p => p.id === +req.params.id)
+    if(address){
+        res.send(address);
+    }else{
+        res.send(404); 
+    }
 })
 
 
